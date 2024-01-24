309be73156033a06ddc1fac9b9a2f404a783cec6
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-01-24 23:21:56 +0000

    removed unnecessary files

--- js/publish_inject.js
+++
@@ -1,8 +0,0 @@
-const publishBtn = document.querySelector('.add-list-publish:not([disabled])');
-
-console.log('starting publish');
-window.confirm = function(){
-    return true;
-}
-publishBtn.click();
-console.log('published link')
