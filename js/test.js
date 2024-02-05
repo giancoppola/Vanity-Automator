d776b2297cce4934e08d484b082c81a35dfbba8d
Gian <giancoppola@hotmail.co.uk>
2024-02-05 17:47:19 +0000

    ready for more testing

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 async function UpdateVanity(ids, type) {
     let promises = [];
     let preview;
-    let rePreview = '.IsPreviewing=(.*)&';
+    let rePreview = '.IsPreviewing=(.{4,5})&';
     let unpreview;
     let reUnpreview = '.IsUnpublishingPreview=(.{4,5})&';
     let publish;
@@ -38,12 +38,38 @@ async function UpdateVanity(ids, type) {
     let bodyStart = `SelectedCompanySiteId=58858&OrganizationId=1554&IsNewFullSite=True&ErrorMessage=&LanguageCode=EN&JobCustomFacetField=ALL&KeywordVanity=&DoubleclickTag=&UtmSource=&UtmMedium=&UtmCampaign=`;
     let bodyEnd = `&__RequestVerificationToken=CfDJ8OGhF19QdBxMqro-CfTj-HlBVexjnDpSbxn6PrtIdrOZouQHSigdtJbm2NQHHWLm0KEi7p7osFawIIt8mfvj52NFo_dHzXLUPSEB3fsLmj07vEUMHQCc-NNMTbonbOBNx3WTos117--u_1SAjmpjU5ICp4-IyRHFINRIBAbx8Kv78_FLWKaOnejkb6eN05mb5w&X-Requested-With=XMLHttpRequest`;
     for(let id of ids){
-        bodyMid += `&VanitySearchUrls.index=${id[0]}&VanitySearchUrls%5B${id[0]}%5D.IsAdded=True&VanitySearchUrls%5B${id[0]}%5D.IsDeleting=False&VanitySearchUrls%5B${id[0]}%5D.IsPublishing=False&VanitySearchUrls%5B${id[0]}%5D.IsPreviewing=False&VanitySearchUrls%5B${id[0]}%5D.IsUnpublishingLive=False&VanitySearchUrls%5B${id[0]}%5D.IsUnpublishingPreview=False&VanitySearchUrls%5B${id[0]}%5D.VanityUrl=${id[1]}&VanitySearchUrls%5B${id[0]}%5D.LanguageCode=EN&VanitySearchUrls%5B${id[0]}%5D.IsPublished=False&VanitySearchUrls%5B${id[0]}%5D.EntityId=84688&VanitySearchUrls%5B${id[0]}%5D.CategoryName=&VanitySearchUrls%5B${id[0]}%5D.CategoryTerm=&VanitySearchUrls%5B${id[0]}%5D.CategoryFacetType=&VanitySearchUrls%5B${id[0]}%5D.LocationName=&VanitySearchUrls%5B${id[0]}%5D.LocationTerm=&VanitySearchUrls%5B${id[0]}%5D.LocationFacetType=&VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldName=&VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldTerm=&VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldValue=&VanitySearchUrls%5B${id[0]}%5D.NewMultiSelectMappingItem=&VanitySearchUrls%5B${id[0]}%5D.VanitySearchUrlMapping=System.Collections.Generic.List%601%5BTmp.TalentBrew.Admin.Core.Models.VanitySearchUrlMappingModel%5D&VanitySearchUrls%5B${id[0]}%5D.DoubleClickTag=&VanitySearchUrls%5B${id[0]}%5D.UtmSource=&VanitySearchUrls%5B${id[0]}%5D.UtmMedium=&VanitySearchUrls%5B${id[0]}%5D.UtmCampaign=`;
+        bodyMid += `&VanitySearchUrls.index=${id[0]}\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsAdded=True\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsDeleting=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsPublishing=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsPreviewing=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsUnpublishingLive=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsUnpublishingPreview=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.VanityUrl=${id[1]}\
+                    &VanitySearchUrls%5B${id[0]}%5D.LanguageCode=EN\
+                    &VanitySearchUrls%5B${id[0]}%5D.IsPublished=False\
+                    &VanitySearchUrls%5B${id[0]}%5D.EntityId=84688\
+                    &VanitySearchUrls%5B${id[0]}%5D.CategoryName=\
+                    &VanitySearchUrls%5B${id[0]}%5D.CategoryTerm=\
+                    &VanitySearchUrls%5B${id[0]}%5D.CategoryFacetType=\
+                    &VanitySearchUrls%5B${id[0]}%5D.LocationName=\
+                    &VanitySearchUrls%5B${id[0]}%5D.LocationTerm=\
+                    &VanitySearchUrls%5B${id[0]}%5D.LocationFacetType=\
+                    &VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldName=\
+                    &VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldTerm=\
+                    &VanitySearchUrls%5B${id[0]}%5D.CustomFacetFieldValue=\
+                    &VanitySearchUrls%5B${id[0]}%5D.NewMultiSelectMappingItem=\
+                    &VanitySearchUrls%5B${id[0]}%5D.VanitySearchUrlMapping=System.Collections.Generic.List%601%5BTmp.TalentBrew.Admin.Core.Models.VanitySearchUrlMappingModel%5D\
+                    &VanitySearchUrls%5B${id[0]}%5D.DoubleClickTag=\
+                    &VanitySearchUrls%5B${id[0]}%5D.UtmSource=\
+                    &VanitySearchUrls%5B${id[0]}%5D.UtmMedium=\
+                    &VanitySearchUrls%5B${id[0]}%5D.UtmCampaign=`;
     }
     let body = bodyStart + bodyMid + bodyEnd;
     for(let id of ids){
         let uBody = (" " + body).slice(1);
-        uBody.replace(`VanitySearchUrls%5B${id[0]}%5D`+rePreview, "True");
+        let re = new RegExp(`(?<=VanitySearchUrls%5B${id[0]}%5D\.IsPreviewing=).{4,5}(?=&)`, 'gm');
+        uBody = uBody.replace(re, "True");
         console.log(uBody);
         promises.push(fetch("https://tbadmin.radancy.net/redirects/savevanitysearchurl", {
             "headers": {
